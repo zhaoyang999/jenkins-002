6fa61c611953dde978ce250a35f83684f503b728
zhaoyang999 <zzy894370071@163.com>
2021-02-04 18:49:51 +0800

    1

---
+++ scripts/test_login.py
@@ -0,0 +1,14 @@
+class TestLogin:
+
+    def test_hello1(self):
+        print('hahhaa')
+        assert True
+
+    def test_hello2(self):
+        assert True
+
+    def test_hello3(self):
+        assert True
+
+    def test_hello4(self):
+        assert True
